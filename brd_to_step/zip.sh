582977b9c54326beb95cbc4f92192867c5917292
Marc Battistello <marc.battistello@gmail.com>
2019-01-04 15:58:05 -0600

    updates to brd to step

--- brd_to_step/zip.sh
+++ brd_to_step/zip.sh
@@ -2,11 +2,12 @@
 
 # Creates a zip file that can be deployed as an AWS Lambda function
 
-zip -r9 step_to_stl_lambda.zip \
+zip -r9 brd_to_step_lambda.zip \
 aws_s3.py \
 conversion_error.py \
 handler.py \
 step_to_stl.py \
+brd_to_step.py \
 lib \
 OCC/*.py \
 OCC/*.so
